727673d53de3d293d755108697065241d2bc2e8f
Samuel Debruyn <s@muel.be>
2014-03-25 13:19:15 +0100

    klassen

---
+++ BBapp/BBapp/src/main/java/be/muel/sa/entities/Address.java
@@ -0,0 +1,21 @@
+package be.muel.sa.entities;
+
+/**
+ * Created by Samuel on 25/03/2014.
+ */
+public class Address {
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    private String naam;
+
+    public Address() {
+
+    }
+}
---
+++ BBapp/BBapp/src/main/java/be/muel/sa/entities/Room.java
@@ -0,0 +1,7 @@
+package be.muel.sa.entities;
+
+/**
+ * Created by Samuel on 25/03/2014.
+ */
+public class Room {
+}
