648919221faa54ba304332e85122474b203fe0e6
Talipov Daniyar <talipov.daniyar@hotmail.com>
2014-05-13 12:10:14 +0200

    POI adapter

--- BBapp/BBapp/src/main/java/be/muel/sa/ui/CustomPOIAdapter.java
+++ BBapp/BBapp/src/main/java/be/muel/sa/ui/CustomPOIAdapter.java
@@ -1,13 +1,18 @@
 package be.muel.sa.ui;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
+import be.muel.sa.R;
 import be.muel.sa.entities.PlaceOfInterest;
+import be.muel.sa.entities.Promotion;
 
 /**
  * Created by DaniyarTalipov on 13/05/14.
@@ -25,9 +30,29 @@ public class CustomPOIAdapter extends ArrayAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
-    }
+        View itemView = convertView;
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+        if (itemView == null)
+            itemView = inflater.inflate(R.layout.poiadapter_row, null);
+
+        PlaceOfInterest placeOfInterest = getListPOIAdapter().get(position);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageViewPromoRoom);
+        TextView tvTitle = (TextView) itemView.findViewById(R.id.textViewTitle);
+        TextView tvDescription = (TextView) itemView.findViewById(R.id.textViewDescription);
+        TextView tvTravelTime = (TextView) itemView.findViewById(R.id.textViewTravelTime);
+
+        tvTitle.setText(placeOfInterest.getName());
+        tvDescription.setText(placeOfInterest.getType().toString());
+        tvTravelTime.setText(placeOfInterest.getType().toString());
+
+        if (placeOfInterest.getPhotos().size() > 0)
+            imageView.setImageBitmap(placeOfInterest.getPhotos().get(0).getBitmap());
+
+        return itemView;
+
+    }
 
     public List<PlaceOfInterest> getListPOIAdapter() {
         return listPOIAdapter;
